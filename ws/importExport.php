2d068d7d540f4133ff784ab00099a969a06304b1
Quentin S <chteuchteu@gmail.com>
2015-06-21 21:25:45 +0200

    Update redirection to include call parameters

--- ws/importExport.php
+++ ws/importExport.php
@@ -1,4 +1,4 @@
 <?php
-header('Location: http://ws.munin-for-android.com/importExport.php');
-?>
-
+header("Status: 301 Moved Permanently");
+header("Location: http://ws.munin-for-android.com/importExport.php?". $_SERVER['QUERY_STRING']);
+exit;
