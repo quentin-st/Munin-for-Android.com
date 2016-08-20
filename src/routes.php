f88c94726f5e9a7c5b687941da120160973c9fcd
Quentin Stoeckel <chteuchteu@gmail.com>
2016-08-20 22:17:40 +0200

    Add i18n page, create base.html.twig

--- src/routes.php
+++ src/routes.php
@@ -1,12 +1,33 @@
 <?php
 // Routes
 
-$app->get('/', function ($request, $response, $args) {
-    var_dump(GCMStats::getViewGCMStats()['hits']);
-
-    // Render index view
+/**
+ * Index
+ */
+$app->get('/', function ($request, $response, $args) use ($app) {
     return $this->view->render($response, 'index.html.twig', [
         'features' => Features::getAll(),
         'gcm_stats' => GCMStats::getViewGCMStats()
     ]);
 });
+
+/**
+ * I18n
+ */
+$app->get('/i18n', function ($request, $response, $args) use ($app) {
+    return $this->view->render($response, 'i18n.html.twig');
+});
+
+
+/**
+ * Redirects
+ */
+$redirects = [
+    '/index.php' => '/',
+    '/i18n.php' => '/i18n'
+];
+foreach ($redirects as $old => $new) {
+    $app->get($old, function($request, $response, $args) use ($app, $old, $new) {
+        return $response->withRedirect($new, 301);
+    });
+}
