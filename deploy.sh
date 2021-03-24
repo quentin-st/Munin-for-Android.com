0c72cdfe6daea284ff8b7984f821b319763081ee
Quentin Stoeckel <stoeckel.quentin@gmail.com>
2021-03-24 18:42:48 +0100

    Remove bower dependency

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function set_acl() {
+    # set ACL on var folder if necessary
+    CURRENT_USER=`whoami`
+    FOLDER="$1"
+    ACL=`getfacl "$FOLDER" | grep -E "($CURRENT_USER|www-data):..x$" -ic`
+    if [[ "$ACL" != 4 ]]; then
+        echo "setting missing ACLs on $FOLDER"
+        setfacl -R -m u:www-data:rwX -m u:"$CURRENT_USER":rwX "$FOLDER" && \
+        setfacl -dR -m u:www-data:rwX -m u:"$CURRENT_USER":rwX "$FOLDER"
+    fi
+}
+set_acl logs
+set_acl cache
+
+composer --ansi -n install --no-dev --optimize-autoloader
+sass public/css/style.scss
